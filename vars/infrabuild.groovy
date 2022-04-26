765679383d2a77c8b5f626394b5fcbb97a209e60
manuelramosb <manuel.ramos@payclip.com>
2022-04-25 20:07:49 -0500

    Added CAPABILITY settings

--- vars/infrabuild.groovy
+++ vars/infrabuild.groovy
@@ -30,7 +30,7 @@ def call() {
            stage('Create Stack'){
                steps {
                   script {
-                    sh 'aws cloudformation deploy --template-file ecs.yaml --stack My-New-ECS --region us-east-1 --parameter-overrides KeyName=test VpcId=vpc-0783368ea2d95fb42 SubnetId=subnet-03dd89fed4d13aa64,subnet-050a9bb98a0c4cab0'
+                    sh 'aws cloudformation deploy --template-file ecs.yaml --stack My-New-ECS --region us-east-1 --parameter-overrides KeyName=test VpcId=vpc-0783368ea2d95fb42 SubnetId=subnet-03dd89fed4d13aa64,subnet-050a9bb98a0c4cab0 --capabilities CAPABILITY_NAMED_IAM CAPABILITY_AUTO_EXPAND'
                   }
                }
            }
